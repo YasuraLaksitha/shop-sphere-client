7b4f561e464dd4830925e9eb35e7faebbd7e172a
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-04-19 13:45:51 +0530

    tailwind theam config

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="">
+    <div className="flex items-center justify-center h-screen bg-gray-800 text-2xl text-white font-bold">
       Shop Sphere
     </div>
   )
